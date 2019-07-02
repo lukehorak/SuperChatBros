3643ba1830c28debc396d21bb6907749f163e4ff
Lukas Horak <lukasbhorak@gmail.com>
2019-07-02 15:29:33 -0400

    added logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,8 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Super CHAT Bros!</a>
+        
+        <a href="/" className="navbar-brand"><img className="navbar-image" src="/styles/SuperChatBrosLogo.png"/></a>
       </nav>
     );
   }
