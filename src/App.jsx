f04aabdcb075147e769eb1aebda1c7e8c141d7bf
Lukas Horak <lukasbhorak@gmail.com>
2019-07-02 10:36:31 -0400

    added mock loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React, {Component} from 'react';
-import  MessageList  from './MessageList.jsx';
-import  Navbar  from './Navbar.jsx';
-import Footer from './Footer.jsx'
+import React, { Component } from 'react';
+import MessageList from './MessageList.jsx';
+import Navbar from './Navbar.jsx';
+import Chatbar from './Chatbar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    // Emulate loading time for now
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 1000)
+  }
+
   render() {
+    const messageList = (this.state.loading ? <h1>loading...</h1> : <MessageList />);
     return (
       <div>
         <Navbar />
-        <MessageList />
-        <Footer />
+        {messageList}
+        <Chatbar />
       </div>
     );
   }
