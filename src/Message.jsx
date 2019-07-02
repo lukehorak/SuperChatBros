4933d8a95a68b62ebf165d5b84f309a00b64cb68
Lukas Horak <lukasbhorak@gmail.com>
2019-07-02 15:29:25 -0400

    abstracted messages to include system messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,13 +3,20 @@ import React from 'react';
 // Private Methods
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 const randomKeyGen = () => {
-  return Math.random().toString()
+  return Math.floor(Math.random() * 1000000).toString()
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Component Function Export
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = function Message(msgObj) {
-  const { username, content } = msgObj;
+  const { username, content, system } = msgObj;
+  if(system){
+    return(
+      <div className="message system" key={randomKeyGen()}>
+        {username} {content}
+      </div>
+    )
+  }
   return (
     <div className="message" key={randomKeyGen()}>
         <span className="message-username">{username}</span>
