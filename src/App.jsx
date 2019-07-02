48d175b569eb190b11779ec0c28fe9457d0c44eb
Lukas Horak <lukasbhorak@gmail.com>
2019-07-02 15:29:04 -0400

    Moved demo msgs to file;  implemented sending msgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 /* eslint-disable no-console */
 import React, { Component } from 'react';
-import MessageList from './MessageList.jsx';
-import Navbar from './Navbar.jsx';
-import Chatbar from './Chatbar.jsx';
-import Message from './Message.jsx';
-import SystemMessage from './SystemMessage.jsx'
-
-const messageData = [
-  { username: 'Pikachu', content: '*Spams Thunder*' },
-  { username: 'Link', content: 'HYAAAAAA' },
-  { username: 'Kirby', content: 'Om nom nom!' }
-  ];
+import MessageList          from './MessageList.jsx';
+import Navbar               from './Navbar.jsx';
+import Chatbar              from './Chatbar.jsx';
+import Message              from './Message.jsx';
+import messageData          from '../demoMessages.json'; // Demo Messages
 
 const getMessages = (msgList) => {
   // TODO [Refactor] - Refactor Message component to generate all types of messages
-  const messages = [<SystemMessage key='s0' />];
-  msgList.forEach(messageObject => {
-    messages.push(Message(messageObject))
-  })
+  const messages = [];
+  msgList.forEach(messageObject => { messages.push(Message(messageObject)) })
   return messages;
 }
 
@@ -33,18 +25,32 @@ class App extends Component {
   }
 
   componentDidMount() {
-
     console.log('componentDidMount <App />');
     setTimeout(() => {
       console.log('Simulating incoming message');
       // Add a new message to the list of messages in the data store
-      const newMessage = <Message key='simmed' username='Cpt. Falcon' content='falconnnnnnn CHAT!' type='message' />
+      const newMessage = Message({ username:'Cpt. Falcon', content:'falconnnnnnn CHAT!' });
       const messages = this.state.messages.concat(newMessage)
-      console.log(messages)
       // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
       this.setState({ messages: messages, loading: false})
     }, 1000);
+  }
+
+  _handleKeyDown = (e) => {
+    if (e.key === 'Enter'){
+      const messageData = {
+        username: e.target.parentNode.firstChild.value,
+        content: e.target.value
+      }
+      //console.log(`${messageData.user} says "${messageData.message}"`);
+      const newIM = Message(messageData);
+      console.log(newIM.props)
+      const messages = this.state.messages.concat(newIM)
+      // Update the state of the app component.
+      this.setState({ messages: messages, loading: false})
+      e.target.value = '';
+      
+    }
   }
 
   render() {
@@ -52,7 +58,7 @@ class App extends Component {
       <div>
         <Navbar />
         <MessageList messages={this.state.messages} loading={this.state.loading} />
-        <Chatbar currentUser={this.state.currentUser} />
+        <Chatbar currentUser={this.state.currentUser} keydown={this._handleKeyDown}/>
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 class Chatbar extends Component {
+
   render() {
     return (
-      <footer className="chatbar">
+      <footer className="chatbar" onKeyDown={this.props.keydown}>
         <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
