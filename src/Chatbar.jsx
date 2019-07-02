4c67a9efc22ab4b354ff899aee7fdb6a47049d42
Lukas Horak <lukasbhorak@gmail.com>
2019-07-02 12:33:58 -0400

    Style changes

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -4,7 +4,7 @@ class Chatbar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Super Chat Bros</a>
+        <a href="/" className="navbar-brand">Super CHAT Bros!</a>
       </nav>
     );
   }
