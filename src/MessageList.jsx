efcf48bad6692607cd9d174f13d208b0a41d8aed
Lukas Horak <lukasbhorak@gmail.com>
2019-07-03 14:03:12 -0400

    swapped out "loading" for connection check

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon }  from '@fortawesome/react-fontawesome'
+import { faBroadcastTower }        from '@fortawesome/free-solid-svg-icons'
 
 class MessageList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { loading: true };
+    //this.state = { loading: true };
   }
 
   render() {
     // Resolve loading time
-    const messages = (this.props.loading ? <h1>loading...</h1> : this.props.messages);
+    const messages = (this.props.connected ? this.props.messages : <FontAwesomeIcon icon={faBroadcastTower}/>);
     return (
       <main className="messages">
         {messages}
